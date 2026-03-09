47553ffb0ae44488866817193fa290d21f01b8a4
Onzoro-rento <rento0725ken1009@icloud.com>
2026-03-09 17:32:49 +0900

    5秒ごとの保存

--- webapp/nextjs/app/editor/page.tsx
+++ webapp/nextjs/app/editor/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { useQuery, useMutation } from '@tanstack/react-query';
 import { useEditor, EditorContent, ReactNodeViewRenderer } from '@tiptap/react';
 import Document from '@tiptap/extension-document';
 import Heading from '@tiptap/extension-heading';
